4718af1c0e0e27ddfc00db4062ed7e3380552eb2
lemnyxx <115400742+lemnyxx@users.noreply.github.com>
2022-12-19 22:52:51 +0800

    Search Bar updated
    Search Bar updated to a more new, flexible code.

--- miow/script.js
+++ miow/script.js
@@ -1,21 +1,20 @@
 var resize = 1 //resize
 
 //search bar
-function searchBar() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("search");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("musicList");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
+// JavaScript code
+function search_animal() {
+	let input = document.getElementById('searchbar').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('animals');
+	
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";				
+		}
+	}
 }
 
 //open
