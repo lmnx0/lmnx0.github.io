f97d9883d3ee23d5cae234c4909d491ccbed7153
lemnyxx <115400742+lemnyxx@users.noreply.github.com>
2022-12-20 13:45:58 +0800

    Added "settings" button
    Added "settings" button. It has no functionality for now.

--- miow/script.js
+++ miow/script.js
@@ -40,6 +40,32 @@ function minNav() {
     document.getElementById("main").style.marginLeft= "350px";
 }
 
+// Get the modal
+var modal = document.getElementById("settings-menu");
+
+// Get the button that opens the modal
+var btn = document.getElementById("settings-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 //backup resize
 /* function clickNav() {
     switch(resize) {
